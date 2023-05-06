b024e74043f34ac9d074de9c9ee9ea3206e1557c
Gil Megidish <gil@megidish.net>
2023-05-06 19:41:22 +0300

    Fixed footer-dialog height, aligning logo to bottom

--- src/components/EditView.tsx
+++ src/components/EditView.tsx
@@ -13,6 +13,8 @@ import {HistoryTabPanel} from "./HistoryTabPanel";
 import {IterationDurationUtils} from "../services/iteration-duration-utils";
 import {SelectWithBlanket} from "./SelectWithBlanket";
 import {DatePickerWithBlanket} from "./DatePickerWithBlanket";
+import SectionMessage from "@atlaskit/section-message";
+import CrossIcon from '@atlaskit/icon/glyph/cross'
 
 export const EditView: FC<{
 	boardService: BoardService;
@@ -26,6 +28,8 @@ export const EditView: FC<{
 	const [datePickerVisible, setDatePickerVisible] = useState(false);
 	const [lengthPickerVisible, setLengthPickerVisible] = useState(false);
 	const [isInvalidGoal, setInvalidGoal] = useState(false);
+	const [showFirstTip, setShowFirstTip] = useState(false);
+	const [showWelcomeToFirstIteration, setShowWelcomeToFirstIteration] = useState(false);
 
 	const handleCloseDialog = () => {
 		AtlassianClient.closeDialog();
@@ -113,10 +117,34 @@ export const EditView: FC<{
 		setLengthPickerVisible(false);
 	};
 
+	const FirstTipView = () => {
+		return (
+			<SectionMessage appearance={"discovery"} title="Tips! 🤔">
+				<ul style={{padding: "0"}}>
+					<li>Keep it short and measurable!</li>
+					<li>Ensure your goals are descriptive!</li>
+					<li>Make sure everybody understand the lingo!</li>
+				</ul>
+				<p>
+					<a href="#" onClick={() => setShowFirstTip(false)}>Gotcha!</a>
+				</p>
+			</SectionMessage>
+		);
+	};
+
 	useEffect(() => {
-		boardService.getConfiguration().then((configuration: AddonConfiguration) => {
-			LoggerService.log("[WebPanel] loaded configuration", configuration);
-			setAddonConfiguration(configuration);
+
+		boardService.getGoalHistory().then(goalHistory => {
+
+			if (goalHistory.iterations.length === 0) {
+				setShowFirstTip(true);
+				setShowWelcomeToFirstIteration(true);
+			}
+
+			boardService.getConfiguration().then((configuration: AddonConfiguration) => {
+				LoggerService.log("[WebPanel] loaded configuration", configuration);
+				setAddonConfiguration(configuration);
+			});
 		});
 
 		const fourWeeksAgo = new Date(+new Date() - 86400 * 4 * 7 * 1000).toISOString();
@@ -125,10 +153,11 @@ export const EditView: FC<{
 	}, []);
 
 	return (
-		<div className="edit-view">
-			<div style={{position: "absolute", right: "1em", fontSize: "1.5em", zIndex: "100"}}>
-				<span style={{cursor: "pointer", color: "black"}} onClick={handleCloseDialog}>&times;</span>
+		<div className="edit-view" style={{position: "relative"}}>
+			<div style={{position: "absolute", right: "1em", zIndex: "100", cursor: "pointer"}} onClick={handleCloseDialog}>
+				<CrossIcon label=""/>
 			</div>
+
 			<div style={{flex: 1}} className="tabs-container">
 				<Tabs id="default">
 					<TabList>
@@ -138,62 +167,78 @@ export const EditView: FC<{
 					</TabList>
 					<TabPanel>
 						{addonConfiguration &&
-							<div style={{marginTop: "1.5em", width: "100%", display: "flex", flexDirection: "column"}}>
-								<div style={{flex: 1}}>
-									<div className={isInvalidGoal ? "field-validation-failed" : ""} onClick={() => setInvalidGoal(false)}>
-										<MutableGoalList list={addonConfiguration.goals} onListChanged={handleListChanged} maxItems={3}/>
+							<div style={{width: "100%", display: "flex", flexDirection: "column"}}>
+								{showWelcomeToFirstIteration &&
+									<div style={{width: "100%", textAlign: "center", marginTop: "0.66em", fontSize: "1.15em"}}>
+										Welcome to your first iteration! 🚀🎉🥰
 									</div>
+								}
+								<div style={{flex: 1, marginTop: "1.25em"}}>
 
-									<div style={{width: "50%", marginTop: "2.5em"}}>
-										<div>
-											<div className="iteration-select-label">
-												Iteration start date:
+									<div style={{display: "flex", flexDirection: "row"}}>
+										<div style={{flex: "2"}}>
+											<div className={isInvalidGoal ? "field-validation-failed" : ""} onClick={() => setInvalidGoal(false)}>
+												<MutableGoalList list={addonConfiguration.goals} onListChanged={handleListChanged} maxItems={3}/>
 											</div>
-											<div className="iteration-select-value" onClick={iterationStartClicked}>
-												<div className={isInvalidDate ? "field-validation-failed" : ""} style={{padding: "4px"}}>
-													{addonConfiguration.iterationStartDate}
+											<div style={{width: "100%", marginTop: "2.5em"}}>
+												<div>
+													<div className="iteration-select-label">
+														Iteration start date:
+													</div>
+													<div className="iteration-select-value" onClick={iterationStartClicked}>
+														<div className={isInvalidDate ? "field-validation-failed" : ""}
+															style={{padding: "4px"}}>
+															{addonConfiguration.iterationStartDate}
+														</div>
+													</div>
+												</div>
+
+												<div>
+													<div className="iteration-select-label">
+														Iteration length:
+													</div>
+													<div className="iteration-select-value" onClick={iterationLengthClicked}>
+														<div className={isInvalidDate ? "field-validation-failed" : ""}
+															style={{padding: "4px"}}>
+															{addonConfiguration.iterationLengthWeeks} Weeks
+														</div>
+													</div>
 												</div>
 											</div>
-										</div>
 
-										<div>
-											<div className="iteration-select-label">
-												Iteration length:
-											</div>
-											<div className="iteration-select-value" onClick={iterationLengthClicked}>
-												<div className={isInvalidDate ? "field-validation-failed" : ""} style={{padding: "4px"}}>
-													{addonConfiguration.iterationLengthWeeks} Weeks
-												</div>
-											</div>
+											{datePickerVisible &&
+												<DatePickerWithBlanket
+													minDate={minimumDateInPicker}
+													defaultValue={addonConfiguration.iterationStartDate}
+													onChange={(value) => onDateChanged(value)}
+													onBlankedClicked={onBlanketClicked}
+												/>
+											}
+
+											{lengthPickerVisible &&
+												<SelectWithBlanket
+													placeholder="Select iteration length"
+													defaultValue={{
+														label: `${addonConfiguration.iterationLengthWeeks} Weeks`,
+														value: addonConfiguration.iterationLengthWeeks
+													}}
+													options={[
+														{label: "1 Week", value: 1},
+														{label: "2 Weeks", value: 2},
+														{label: "3 Weeks", value: 3},
+														{label: "4 Weeks", value: 4},
+													]}
+													onChange={onIterationLengthChanged}
+													onBlanketClicked={onBlanketClicked}
+												/>
+											}
+										</div>
+										<div style={{flex: "1"}}>
+											{showFirstTip &&
+												<FirstTipView/>
+											}
 										</div>
 									</div>
-
-									{datePickerVisible &&
-										<DatePickerWithBlanket
-											minDate={minimumDateInPicker}
-											defaultValue={addonConfiguration.iterationStartDate}
-											onChange={(value) => onDateChanged(value)}
-											onBlankedClicked={onBlanketClicked}
-										/>
-									}
-
-									{lengthPickerVisible &&
-										<SelectWithBlanket
-											placeholder="Select iteration length"
-											defaultValue={{
-												label: `${addonConfiguration.iterationLengthWeeks} Weeks`,
-												value: addonConfiguration.iterationLengthWeeks
-											}}
-											options={[
-												{label: "1 Week", value: 1},
-												{label: "2 Weeks", value: 2},
-												{label: "3 Weeks", value: 3},
-												{label: "4 Weeks", value: 4},
-											]}
-											onChange={onIterationLengthChanged}
-											onBlanketClicked={onBlanketClicked}
-										/>
-									}
 								</div>
 							</div>
 						}
@@ -213,7 +258,6 @@ export const EditView: FC<{
 			<div className="dialog-footer">
 				<div style={{display: "flex"}}>
 					{addonConfiguration &&
-
 						<div style={{flex: "50%", marginTop: "auto"}}>
 							<Button appearance={"primary"} onClick={onReadyToStartClicked} isDisabled={showSpinner}>
 								Ready to Start?
@@ -224,7 +268,7 @@ export const EditView: FC<{
 						</div>
 					}
 
-					<div style={{flex: "50%"}}>
+					<div style={{flex: "50%", marginTop: "auto"}}>
 						<BetterGoalsLogo/>
 					</div>
 				</div>
