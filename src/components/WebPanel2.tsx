886b51ba073342757e3b7802f60f1f8de8687117
Gil Megidish <gil@megidish.net>
2023-08-28 23:09:43 +0200

    Improved support for enhanced board

--- src/components/WebPanel2.tsx
+++ src/components/WebPanel2.tsx
@@ -1,19 +1,31 @@
-import {FC, ReactElement, useEffect, useState} from "react";
-import React from "react";
-import {WebPanel} from "./WebPanel";
+import React, {FC, ReactElement, useEffect, useState} from "react";
 import {QueryParserService} from "../services/query-parser-service";
+import {AtlassianClient} from "../services/atlassian-client";
 
 export const WebPanel2: FC = (): ReactElement => {
 
 	const [lic, setLic] = useState<string>("none");
 
+	const callbackFunc = (e: any) => {
+		// nope;
+		console.log("callback called", e);
+	};
+
 	useEffect(() => {
 		const parsed = QueryParserService.parse(document.location.search);
-		const lic = parsed.get("lic") || "none";
+		// const lic = parsed.get("lic") || "none";
+		const lic = "active";
 		setLic(lic);
+
+		AtlassianClient.createDialog({
+			key: "better-goals-web-panel-dialog",
+			width: "800px",
+			height: "400px",
+			chrome: false,
+		}).on("close", callbackFunc);
 	}, []);
 
 	return (
-		<WebPanel lic={lic}></WebPanel>
+		<div></div>
 	);
 };
